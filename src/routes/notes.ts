595859455042d189b2d9c3c79abd7afa9ebb3163
Joed Parin <parinjoed@gmail.com>
2019-11-12 17:26:43 +0800

    feat: Added endpoint for note create/insert

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { list } from '../controllers/notes';
+import { create } from '../controllers/notes';
 const noteRouter: Router = Router();
 
 noteRouter.get('/', list);
+noteRouter.post('/', create);
 
 export { noteRouter };
