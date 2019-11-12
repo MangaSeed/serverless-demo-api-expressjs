1a169cb6c757173daf9871f77fe9e11c78291222
Joed Parin <parinjoed@gmail.com>
2019-11-12 15:15:49 +0800

    refact: update joi a data validation library to a supported version

--- src/services/note.ts
+++ src/services/note.ts
@@ -2,14 +2,11 @@ import AWS = require('aws-sdk');
 import uuid from 'uuid';
 import Joi, {
   string,
-  number,
   object
-} from "joi";
+} from "@hapi/joi";
 import { ResponseDefaultType } from '../types/responseDefaultType';
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-
 
 const listNotes = (data: any, cb: (arg0: ResponseDefaultType) => void) => {
 
@@ -26,9 +23,9 @@ const listNotes = (data: any, cb: (arg0: ResponseDefaultType) => void) => {
     }).unknown(true)
   }).unknown(true);
 
-  const valid = Joi.validate(rparam, schema); // validate params
+  const { error, value } = schema.validate(rparam);
 
-  valid.then(val => {
+  if (error === undefined) {
 
     const queryParams = {
       TableName: process.env.NOTES_TABLE,
@@ -39,11 +36,11 @@ const listNotes = (data: any, cb: (arg0: ResponseDefaultType) => void) => {
     }
 
     const res: ResponseDefaultType = { status: 200, message: '' }; // set default value
-    dynamoDb.query(queryParams, (error, result) => {
+    dynamoDb.query(queryParams, (err, result) => {
 
-      if (error) {
-        res.status = error.statusCode,
-          res.message = error.message
+      if (err) {
+        res.status = err.statusCode,
+          res.message = err.message
       } else {
         res.status = 200;
         res.data = result.Items;
@@ -51,17 +48,16 @@ const listNotes = (data: any, cb: (arg0: ResponseDefaultType) => void) => {
       }
 
       cb(res);
+
     });
-  }).catch(reason => {
+  } else {
     const response: ResponseDefaultType = {
       status: 400,
-      message: reason
+      message: "Invalid parameters"
     }
+    console.log(error.details); // logs
     cb(response);
-  });
-
-
+  }
 }
-
 
 export { listNotes }
